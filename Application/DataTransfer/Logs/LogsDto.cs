359d1f78c04b81ecdb58739215947cb9bc8ac250
Stefan <55162040+StefanVaskovic@users.noreply.github.com>
2021-06-23 21:26:20 +0200

    Dodat LogsDto.cs file

---
+++ Application/DataTransfer/Logs/LogsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DataTransfer.Logs
+{
+    public class LogsDto : Dto
+    {
+        public string UserIdentity { get; set; }
+        public int UseCaseId { get; set; }
+        public DateTime Date { get; set; }
+        public string Data { get; set; }
+    }
+}
